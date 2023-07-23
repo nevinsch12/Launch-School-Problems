4690830465b3f450019b01b79d7ddd60f7be7ace
nevinsch12 <christopher.nevins@icloud.com>
2023-07-23 17:41:26 -0500

    Small Problems Easy 1

--- stringy.rb
+++ stringy.rb
@@ -1,5 +1,5 @@
 def stringy(length, alt=1)
-
+	numbers = []
 	length.times do |index|
 		number = index.even? ? 1 : 0
 		number = index.even? ? 0 : 1 if alt == 0
@@ -8,3 +8,9 @@ def stringy(length, alt=1)
 
 	numbers.join
 end	
+puts stringy(19, 0)
+
+puts stringy(6) == '101010'
+puts stringy(9) == '101010101'
+puts stringy(4) == '1010'
+puts stringy(7) == '1010101'
