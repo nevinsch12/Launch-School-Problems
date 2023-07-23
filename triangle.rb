dd9a61405a34b7f258589a045b6d6e8c5eea7a18
nevinsch12 <christopher.nevins@icloud.com>
2023-07-23 17:57:31 -0500

    Small Problems Easy 1

--- triangle.rb
+++ triangle.rb
@@ -1,10 +1,10 @@
 def triangle(number)
   asterisk = "*"
-    if number == 0
-      puts ""
-    else
-      puts "#{asterisk * number}"
-      puts triangle(number - 1)
-    end
+  if number == 0
+    puts ""
+  else
+    puts "#{asterisk * number}"
+    puts triangle(number - 1)
+  end
 end
 triangle(100)
