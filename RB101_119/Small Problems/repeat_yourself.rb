95356364f24c5ca2f102d05b1dd8926d7f7c7d01
nevinsch12 <christopher.nevins@icloud.com>
2023-07-24 12:44:10 -0500

    Easy 1 and 2

---
+++ RB101_119/Small Problems/repeat_yourself.rb
@@ -0,0 +1,4 @@
+def repeat(word, num)
+  num.times { |s| puts word }
+end
+repeat('Hello', 3)
---
+++ RB101_119/Small Problems/triangle.rb
@@ -0,0 +1,11 @@
+def triangle(num)
+  spaces = num - 1
+  stars = 1
+
+  num.times do |n|
+    puts (' ' * spaces) + ('*' * stars)
+    spaces -= 1
+    stars += 1
+  end
+end
+triangle(10)
